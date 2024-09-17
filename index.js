4ceb33e217e695d9a0fcd90979f8e6bb0aae0334
Gaba <xaviergaba@gmail.com>
2024-09-17 12:38:19 +0200

    index update, contact validations complete

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const port = 3000;
 
 app.use(express.static('public'));
 
+app.use(express.urlencoded({ extended: false }));
+
 app.get('/', (req, res)=>{
     res.render('index.ejs')
 });
@@ -24,3 +26,32 @@ app.get('/contact', (req, res)=>{
 app.listen(port, ()=>{
     console.log(`Listening on port ${port}.`)
 });
+
+app.post('/contact', (req, res)=>{
+    const {email, subject, emailContent} = req.body;
+
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+    if(!emailRegex.test(email)){
+        return res.status(400).json({error: 'Incorrect email format'})
+    };
+
+    if(!subject || subject.trim() === ''){
+        return res.status(400).json({error: 'Subject cannot be empty'})
+    }
+
+    if(!emailContent || emailContent.trim() === ''){
+        return res.status(400).json({error: 'Content cannot be empty'})
+    }
+
+    res.status(200).json({
+        message: 'Form sended correctly',
+        data: {
+            email: email,
+            subject: subject,
+            content: emailContent
+        }
+    })
+
+    console.log(email, subject, emailContent)
+})
