a6a35543920c9d0616b2b5a07fa019692ef16edf
Gaba <xaviergaba@gmail.com>
2024-09-17 14:25:31 +0200

    C:/Program Files/Git/opinion must be ended

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ import express from 'express'
 const app = express();
 const port = 3000;
 
+let opinions = []
+
 app.use(express.static('public'));
 
 app.use(express.urlencoded({ extended: false }));
@@ -16,7 +18,7 @@ app.get('/blog', (req, res)=>{
 });
 
 app.get('/opinion', (req, res)=>{
-    res.render('opinion.ejs')
+    res.render('opinion.ejs', {opinions})
 });
 
 app.get('/contact', (req, res)=>{
@@ -54,4 +56,20 @@ app.post('/contact', (req, res)=>{
     })
 
     console.log(email, subject, emailContent)
+})
+
+
+app.post('/opinion', (req, res)=>{
+    const {opinionTextArea} = req.body;
+
+    console.log(opinionTextArea)
+
+    if(!opinionTextArea || opinionTextArea.trim() === ''){
+        return res.redirect('/opinion?error=Opinion%20submit%20empty');
+    };
+
+    opinions.push(opinionTextArea);
+    console.log(opinions)
+
+    return res.redirect('/opinion');
 })
